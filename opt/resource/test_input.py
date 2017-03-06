6e5d46bacaaae6d6c518706eeeeade08fa09e830
Felix Weirich <felix.weirich@hotmail.de>
2017-03-06 14:37:49 +0100

    readded tests

---
+++ opt/resource/test_input.py
@@ -0,0 +1,28 @@
+import unittest
+import input
+from unittest.mock import patch
+from concourse_common import testutil
+import json
+
+class TestInput(unittest.TestCase):
+
+    @patch("input.file_io")
+    def test_call_to_fileIO(self, mock_io):
+        test = "test/test"
+        testutil.put_stdin(json.dumps({"source": {"prefix": "test"}, "version": {"version": "1.1.0"}}))
+        input.execute(test)
+        mock_io.write_to_file.assert_any_call(test + "/default", "test_1_1_0")
+        mock_io.write_to_file.assert_any_call(test + "/heroku", "test-1-1-0")
+
+    def test_invalid_prefix(self):
+        test = "test/test"
+        testutil.put_stdin(json.dumps({"source": {"prefix": "tes2234"}, "version": {"version": "1.1.0"}}))
+        self.assertEquals(input.execute(test), -1)
+
+    @patch("input.file_io")
+    @patch("input.json")
+    def test_call_to_json(self, mock_io, mock_io2):
+        test = "test/test"
+        testutil.put_stdin(json.dumps({"source": {"prefix": "test"}, "version": {"version": "1.1.0"}}))
+        input.execute(test)
+        mock_io.dumps.assert_any_call({"version": {"version": "1.1.0"}})
---
+++ opt/resource/test_out.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest.mock import patch
+import json
+import out
+from concourse_common import testutil
+
+
+class TestInput(unittest.TestCase):
+
+    @patch("out.file_io")
+    @patch("out.json")
+    def test_json_output(self, mock_io, mock):
+        testutil.put_stdin(json.dumps({"source": {"prefix": "test"}, "params": {"version": "1.1.0"}}))
+        mock.read.return_value = "1.1.0"
+        out.execute("")
+        mock_io.dumps.assert_any_call({"version": {"version": "1.1.0"}})
+
+
+if __name__ == '__main__':
+    unittest.main()
