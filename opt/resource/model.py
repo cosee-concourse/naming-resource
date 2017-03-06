6d48bdfc8d0310c9cfef0adcb96d956caa4ec585
Felix Weirich <felix.weirich@hotmail.de>
2017-03-06 14:16:35 +0100

    included concource common package

--- opt/resource/model.py
+++ opt/resource/model.py
@@ -1,5 +1,4 @@
-from concourse_common import common
-from enum import Enum
+from concourse_common import jsonutil
 
 VERSION_JSON_NAME = 'version'
 
@@ -7,7 +6,7 @@ VERSION_JSON_NAME = 'version'
 class Model:
 
     def __init__(self):
-        self.payload = common.get_payload()
+        self.payload = jsonutil.load_payload()
 
     def get_version_file(self):
         version = self.payload['params']['version']
@@ -22,9 +21,3 @@ class Model:
         if prefix.isalpha():
             return prefix
         return ""
-
-
-class Request(Enum):
-    CHECK = 1
-    IN = 2
-    OUT = 3
--- opt/tests/test_out.py
+++ opt/tests/test_out.py
@@ -1,10 +1,8 @@
-
 import unittest
 from unittest.mock import patch
 import json
-
 import out
-from concourse_common import test_common
+from concourse_common import testutil
 
 
 class TestInput(unittest.TestCase):
@@ -12,7 +10,7 @@ class TestInput(unittest.TestCase):
     @patch("out.file_io")
     @patch("out.json")
     def test_json_output(self, mock_io, mock):
-        test_common.put_stdin(json.dumps({"source": {"prefix": "test"}, "params": {"version": "1.1.0"}}))
+        testutil.put_stdin(json.dumps({"source": {"prefix": "test"}, "params": {"version": "1.1.0"}}))
         mock.read.return_value = "1.1.0"
         out.execute("")
         mock_io.dumps.assert_any_call({"version": {"version": "1.1.0"}})
